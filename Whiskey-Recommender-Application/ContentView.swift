e01aba5da0b94ed62487054f3931d00746f30ce5
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2024-05-31 14:29:01 -0400

    Created SwiftUI App

---
+++ Whiskey-Recommender-Application/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Whiskey-Recommender-Application
+//
+//  Created by Nick_Carroll_Engrain_Partner on 5/31/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Whiskey-Recommender-Application/Whiskey_Recommender_ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whiskey_Recommender_ApplicationApp.swift
+//  Whiskey-Recommender-Application
+//
+//  Created by Nick_Carroll_Engrain_Partner on 5/31/24.
+//
+
+import SwiftUI
+
+@main
+struct Whiskey_Recommender_ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
